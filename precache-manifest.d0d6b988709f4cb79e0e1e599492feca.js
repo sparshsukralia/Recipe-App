a41e6bb5ca363e8a71e9e4a06c0237ab0abae214
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-06-23 01:28:27 +0545

    Updates

---
+++ precache-manifest.d0d6b988709f4cb79e0e1e599492feca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1480742bcc92e3aa94f613d17484238d",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "f1707c5c9ab31501afd1",
+    "url": "/Recipe-App/static/css/main.8d2983de.chunk.css"
+  },
+  {
+    "revision": "d4e4bb17b816bea0c842",
+    "url": "/Recipe-App/static/js/2.62e0ce75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-App/static/js/2.62e0ce75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1707c5c9ab31501afd1",
+    "url": "/Recipe-App/static/js/main.a372a797.chunk.js"
+  },
+  {
+    "revision": "ea20fe3ef1e4ac5df8f2",
+    "url": "/Recipe-App/static/js/runtime-main.6b8c04eb.js"
+  }
+]);
